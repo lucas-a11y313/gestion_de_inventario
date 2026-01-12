a07ae8a21e1619538137b270a63fa28a29b8104c
Lucas_CG <lucascoronel00720@gmail.com>
2026-01-12 08:54:48 -0300

    Creación de la tabla pivot producto_ubicacion

---
+++ database/migrations/2025_12_15_200300_create_producto_ubicacion_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('producto_ubicacion', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('producto_id')->constrained('productos')->onDelete('cascade');
+            $table->foreignId('ubicacion_id')->constrained('ubicaciones')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('producto_ubicacion');
+    }
+};
